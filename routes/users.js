b83e26e2f81144d8db5e5577f91ae065f6ed5de4
DarkYoung <16302010059@fudan.edu.cn>
2019-05-11 01:12:51 +0800

    更新用户相关mock数据

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,8 @@ random.extend({
     const genders = ['男', '女', '其他', '保密'];
     return this.pick(genders);
   }
-})
+});
+
 const getUserInfo = (phoneNumber) => {
   return Mock.mock({
     'errorCode': 0,
@@ -30,6 +31,13 @@ const getUserInfo = (phoneNumber) => {
   });
 };
 
+router.post('/get_user_info', (req, res, next) => {
+  const {
+    access_token
+  } = req.body;
+  res.json(getUserInfo(18717723912));
+});
+
 /* 获取用户信息，':'代表通配符 */
 router.get('/:phoneNumber', function (req, res, next) {
   const {
@@ -38,4 +46,131 @@ router.get('/:phoneNumber', function (req, res, next) {
   res.json(getUserInfo(phoneNumber));
 });
 
+//登录
+router.post('/login', (req, res, next) => {
+  const {
+    user_phone,
+    password,
+    user_agent
+  } = req.body;
+  res.json(getUserInfo(user_phone));
+});
+
+//注册--验证
+router.post('/verify', (req, res, next) => {
+  const {
+    user_phone,
+    password,
+    name,
+    verify_code,
+    user_agent,
+    gender
+  } = req.body;
+  res.json(getUserInfo(user_phone));
+});
+
+//注册--获取验证码
+router.post('/acquire_verify_code', (req, res, next) => {
+  const {
+    user_phone
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+
+//忘记密码--验证
+router.post('/verify_for_forget', (req, res, next) => {
+  const {
+    user_phone,
+    password,
+    verify_code,
+    user_agent
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+
+//忘记密码--获取验证码
+router.post('/acquire_code_for_forget', (req, res, next) => {
+  const {
+    user_phone
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+
+//修改手机号--验证
+router.post('/verify_for_modify', (req, res, next) => {
+  const {
+    user_phone,
+    new_user_phone,
+    verify_code,
+    user_agent,
+    access_token
+  } = req.body;
+  res.json(Mock.mock({
+    'errorCode': 0,
+    'message': '成功',
+    'data': {
+      'accessToken': '@string(64)',
+      'refreshToken': '@string(64)'
+    }
+  }));
+});
+
+// 修改手机号--获取验证码
+router.post('/acquire_code_for_modify', (req, res, next) => {
+  const {
+    user_phone,
+    new_user_phone,
+    access_token
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+//修改密码--验证
+router.post('/modify_password', (req, res, next) => {
+  const {
+    user_phone,
+    original_password,
+    password,
+    access_token
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+
+router.post('/logout', (req, res, next) => {
+  const {
+    access_token
+  } = req.body;
+  res.json({
+    'errorCode': 0,
+    'message': 'success'
+  });
+});
+
+router.post('/refresh_token', (req, res, next) => {
+  const {
+    refresh_token,
+    user_agent
+  } = req.body;
+  res.json(Mock.mock({
+    'errorCode': 0,
+    'message': 'success',
+    'data': {
+      'access_token': '@string(64)'
+    }
+  }));
+});
 module.exports = router;
