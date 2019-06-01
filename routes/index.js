a038f1e2a190ea7a28654308b788f9440751cf28
DarkYoung <16302010059@fudan.edu.cn>
2019-06-01 18:00:06 +0800

    更改请求方式

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.get('/api/community_road', function (req, res, next) {
   res.json(getCommunityRoadInfo());
 });
 
-router.get('/api/part_a/info/:id', function (req, res, next) {
+router.post('/api/part_a/info/:id', function (req, res, next) {
   const {
     id
   } = req.params;
